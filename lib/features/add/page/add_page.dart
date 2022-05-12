67fa93aa6a7e6db11121aa12a9bf0a1a494412a4
Sebastian Sobieszek <sebasob97@gmail.com>
2022-05-13 00:47:09 +0200

    Implement item Model

--- lib/features/add/page/add_page.dart
+++ lib/features/add/page/add_page.dart
@@ -1,6 +1,5 @@
 import 'package:cantwait28/features/add/cubit/add_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddPage extends StatefulWidget {
